639b0eb63c61641a80e07355d5d509c7e820fd0d
Jeevan-Mano <jeevankumar.mano@gmail.com>
2025-05-22 18:44:51 +0530

    17th May 2025 - Part A

---
+++ src/ex_06_Ternary_Operator/Lab062_TO.java
@@ -0,0 +1,13 @@
+package ex_06_Ternary_Operator;
+
+public class Lab062_TO {
+    public static void main(String[] args) {
+
+        //  ? :
+        int age = 17;
+        String can_i_go_to_goa =  age > 18 ? "Yes, You can" : "You can't";
+        System.out.println(can_i_go_to_goa);
+
+
+    }
+}
---
+++ src/ex_06_Ternary_Operator/Lab064_Interview_Ready_Question.java
@@ -0,0 +1,14 @@
+package ex_06_Ternary_Operator;
+
+public class Lab064_Interview_Ready_Question {
+    public static void main(String[] args) {
+
+        // Nested Ternary
+        // result = condition1 ? expression1 : (condition2 ? expression2 : expression3);
+        int number = 26;
+        String result = (number > 18) ? (number > 25 ? "You can drink in Goa" : "You can't Drink") : "You can't go to GOA";
+        System.out.println(result);
+
+
+    }
+}
