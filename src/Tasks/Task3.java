76a85bc31c2744ca337db547466b31b090f4ae9b
Jeevan-Mano <jeevankumar.mano@gmail.com>
2025-05-29 09:49:12 +0530

    24th May 2025 - Part A

---
+++ src/Tasks/Task3.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("dcsd");
+
+        int grade = scanner.nextInt();
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent");
+        }
+
+    }
+}
