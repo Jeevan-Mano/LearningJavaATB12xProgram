9fc44c318932b5c9dda6141f2ac626181ec43dc8
Jeevan-Mano <jeevankumar.mano@gmail.com>
2025-06-03 09:13:45 +0530

    24th May 2025 - Part B

---
+++ src/ex_10_For_Loop/Lab111_For_Loop_Break.java
@@ -0,0 +1,17 @@
+package ex_10_For_Loop;
+
+public class Lab111_For_Loop_Break {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 50; i++) { // 0 to 49, 50
+            // System.out.println(i); 0,1,2,3,4,5
+            if (i == 5){
+                break;
+                // System.out.println(i); 5
+            }
+            System.out.println(i); // 0,1,2,3,4
+        }
+
+
+    }
+}
---
+++ src/ex_10_For_Loop/Lab112_For_loop_Continue.java
@@ -0,0 +1,16 @@
+package ex_10_For_Loop;
+
+public class Lab112_For_loop_Continue {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+
+            if (i == 10){
+                continue;
+            }
+            System.out.println(i);
+            
+        }
+
+    }
+}
